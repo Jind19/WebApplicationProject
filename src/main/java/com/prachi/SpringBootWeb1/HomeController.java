fb0bd38752cc94b751e38a8928e6efe698fbce12
Prachi Jindal <prachibansal135@gmail.com>
2024-05-08 16:09:14 +0200

    Web App with RequestParam mvc prefix suffix

--- src/main/java/com/prachi/SpringBootWeb1/HomeController.java
+++ src/main/java/com/prachi/SpringBootWeb1/HomeController.java
@@ -3,6 +3,7 @@ package com.prachi.SpringBootWeb1;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,16 +13,16 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         System.out.println("home method called");
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num1, @RequestParam("num1") int num2, HttpSession session){
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
 
-        int result = num1 + num2 + 10;
+        int result = num1 + num2;
 
-        session.setAttribute("result", result);
+        model.addAttribute("result", result);
 
-        return "result.jsp";
+        return "result";
     }
 }
